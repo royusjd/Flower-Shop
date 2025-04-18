8120fec2ba73bae446757565bfd7ba5fd8bef1f0
royusjd <juodeikisrojus2005@gmail.com>
2025-04-18 12:11:15 +0300

    Hot section visually seen now

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,5 +1,22 @@
+import Button from "./Button";
 import "./components_style/PlantCard.css";
 
-export default function PlantCard() {
-	return <div>PlantCard</div>;
+export default function PlantCard({ plantId, title, imgSrc, price }) {
+	return (
+		<div className="plant-card">
+			<a href="">
+				<div className="img-box">
+					<img
+						src={imgSrc}
+						alt={title}
+					/>
+				</div>
+				<div className="card-text">
+					<h3 className="plant-title">{title}</h3>
+					<div className="plant-price">{price}</div>
+				</div>
+			</a>
+			<Button colorName={"green-btn"}>Buy</Button>
+		</div>
+	);
 }
--- src/components/PlantCardsList.jsx
+++ src/components/PlantCardsList.jsx
@@ -1,17 +1,38 @@
 import { usePlants } from "../hooks/usePlants";
 import DataHandler from "../utils/DataHandler";
 import "./components_style/PlantCardsList.css";
-import useFetch from "../hooks/useFech";
+import PlantCard from "./PlantCard";
 
 export default function PlantCardsList({ filterType, limit }) {
 	const { plants, loading, error } = usePlants();
+
 	console.log(plants);
+
+	const getFilteredPlants = (plants) => {
+		let filtered = plants;
+		if (filterType === "hot") {
+			filtered = filtered.filter((plant) => plant.rating === 5);
+		}
+		return limit ? filtered.slice(0, limit) : filtered;
+	};
+	const filteredPlants = getFilteredPlants(plants);
+	console.log(filteredPlants);
+
 	return (
 		<div className="plant-cards-list">
 			<DataHandler
 				loading={loading}
 				error={error}
-			></DataHandler>
+			>
+				{filteredPlants.map((plant) => (
+					<PlantCard
+						key={plant.id}
+						title={plant.title}
+						price={plant.price}
+						imgSrc={plant.imgSrc}
+					/>
+				))}
+			</DataHandler>
 		</div>
 	);
 }
