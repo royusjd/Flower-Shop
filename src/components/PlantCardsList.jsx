90b227121c451c2772fcf46b96bd2b40c04cd49e
royusjd <juodeikisrojus2005@gmail.com>
2025-04-18 11:37:25 +0300

    Made some bug fixes

--- src/components/PlantCardsList.jsx
+++ src/components/PlantCardsList.jsx
@@ -4,16 +4,17 @@ import "./components_style/PlantCardsList.css";
 import useFetch from "../hooks/useFech";
 
 export default function PlantCardsList({ filterType, limit }) {
-	// const { plants, loading, error } = usePlants();
-	const {
-		data: plants,
-		loading,
-		error,
-	} = useFetch(
-		"https://gist.githubusercontent.com/royusjd/79c90e9707b13d9425f92d38fdca1a10/raw/11812b9155333f079e4ff826221fe31a16d2e40b/plant.json",
-		"plants",
-		"image"
-	);
+	const { plants, loading, error } = usePlants();
+	// const {
+	// 	data: plants,
+	// 	loading,
+	// 	error,
+	// } = useFetch(
+	// 	"https://gist.githubusercontent.com/royusjd/79c90e9707b13d9425f92d38fdca1a10/raw/11812b9155333f079e4ff826221fe31a16d2e40b/plant.json",
+	// 	"plants",
+	// 	"image"
+	// );
+	console.log(plants);
 	return (
 		<div className="plant-cards-list">
 			<DataHandler
